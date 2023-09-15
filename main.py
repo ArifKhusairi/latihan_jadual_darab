8d161fbb4c843c06dceb5ae2f148498ec9949499
ArifKhusairi <soyayos93@gmail.com>
2023-09-15 21:44:13 +0800

    update
    update

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
 def main():
-    for i in range(55):
-        print("*", end="")
-    print()
-    print("\t\t\t\t\t" + "JADUAL DARAB")
-    for i in range(55):
+    for f in range(100):
         print("*", end="")
     print()
 
-    for j in range(1,13):
-        print("\t\t", j, end="")
-    print("\n\n")
+    for r in range(10):
+        print("\t", end="")
+    print("JADUAL DARAB")
 
-    num1 = 1
-    result1 = 1
-    for p in range(1,13):
-        num1 = result1 * p
-        print("\t\t", num1, end="")
-    print("\n\n")
+    for f in range(100):
+        print("*", end="")
+    print()
 
-    num2 = 2
-    result2 = 0
-    num3 = 3
-    result3 = 0
-    for n in range(1,13):
-        result2 = num2 * n
-        print("\t\t", result2, end="")
-    print("\n\n")
+    for y in range(1, 13):
+        print("\t\t", y, end="")
+    print("\n")
 
-    for r in range(1,13):
-        result3 = num3 * r
-        print("\t\t", result3, end="")
-    print("\n\n")
-
-    for yahoo in range(1,13):
-        print(yahoo, "\n")
-
-
-
-
-
-
-
+    for i in range(1, 13):
+        print(i, "\t\t", end="")
+        for j in range(1, 13):
+            result = i * j
+            print("", result, "", "\t", end="")
+        print()
 
 
 if __name__ == '__main__':
